f443e8bdadddfbc11090a00259a55fd8760d4aaa
Shravan Chaudhari <shravanchaudhari99@gmail.com>
2021-06-26 21:25:42 +0530

    2 commit

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -6,8 +6,15 @@ function SearchForm() {
     <Container>
       <h4>Get the Details</h4>
       <form>
-        <input placeholder="Enter the patient name" type="text" />
-        <input placeholder="Enter ther phone no." type="tel" />
+        <label>
+          Name: <br />
+          <input placeholder="Enter the patient name" type="text" />
+        </label>
+        <label>
+          Phone Number:
+          <br />
+          <input placeholder="Enter ther phone no." type="tel" />
+        </label>
         <button type="submit">Search</button>
       </form>
     </Container>
@@ -21,11 +28,16 @@ const Container = styled.div`
   flex-direction: column;
   position: absolute;
   top: 60px;
-  background-color: white;
+  background-color: #ffffff;
   padding: 40px;
+  width: 50%;
+  border: 4px solid grey;
+  border-radius: 12px;
   form {
+    width: 100%;
     display: flex;
     flex-direction: column;
+    align-items: center;
   }
   h4 {
     margin-bottom: 24px;
@@ -33,14 +45,38 @@ const Container = styled.div`
     font-family: "Montserrat", sans-serif;
     font-size: 40px;
   }
+  label {
+    cursor: pointer;
+    font: 20px "opensans-bold", sans-serif;
+    color: #2f3131;
+    margin-bottom: 5px;
+  }
   input {
-    height: 40px;
-    width: 50vw;
-    border-radius: 5px;
-    margin-bottom: 50px;
-    background-color: white;
+    cursor: pointer;
+  width: 500px;
+  padding: 8px 8px;
+  color: #000;
+  margin-bottom: 10px;
+  border: 0;
+  outline: none;
+  font-size: 15px;
+  line-height: 24px;
+  border-radius: 10px;
+  border: 2px solid grey;
+  margin-bottom: 30px;
+  margin-top: 10px;
+}
   }
   button {
-    background-color: white;
-  }
+    width: 220px;
+  height: 65px;
+  border: none;
+  outline: none;
+  color: #fff;
+  background: #1e4775;
+  cursor: pointer;
+  position: relative;
+  z-index: 0;
+  border-radius: 10px;
+  font-size: 20px;
 `;
